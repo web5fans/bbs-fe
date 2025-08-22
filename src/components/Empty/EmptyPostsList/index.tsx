b209a592b09c69ff4b7729e576ace52737ae71e0
chenhui <274241365@qq.com>
2025-08-22 09:29:33 +0800

    fix: 帖子列表空状态按钮禁用状态

--- src/components/Empty/EmptyPostsList/index.tsx
+++ src/components/Empty/EmptyPostsList/index.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import S from './index.module.scss'
 import Button from "@/components/Button";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 export function EmptyPostsList(props: { goPublish: () => void }) {
+  const { isWhiteUser } = useCurrentUser()
+
   return <div className={S.empty}>
     <img src={'/assets/wink.png'} alt={'wink'} />
     <p className={S.emptyTip}>暂无帖子，快去抢占本版区沙发吧！</p>
-    <Button type={'primary'} className={S.emptyButton} onClick={props.goPublish}>去发布</Button>
+    <Button
+      type={'primary'}
+      disabled={!isWhiteUser}
+      className={S.emptyButton}
+      onClick={props.goPublish}>去发布</Button>
   </div>
 }
