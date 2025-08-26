6b88e523943533565f271b312e42e25f1e7c8c3c
chenhui <274241365@qq.com>
2025-08-26 10:05:41 +0800

    fix: 主站轮播文案

--- src/app/posts/_components/NoticeBoard/index.tsx
+++ src/app/posts/_components/NoticeBoard/index.tsx
@@ -41,14 +41,14 @@ const NoticeBoard = () => {
         >
           欢迎你&nbsp;
           <span className={'capitalize'}>{userProfile?.displayName}</span>
-          ！这是你第 1 次来到 BBS 社区，目前论坛有 - 人与你同在，有 - 人曾与你共享同一个网络～
+          ！这是你第 1 次来到 BBS 社区，目前论坛有 0 人与你同在，有 0 人曾与你共享同一个网络～
         </p> : <p
           style={{ marginRight: 20 }}
           className={S.marquee}
         >
           欢迎你&nbsp;
           <span className={'capitalize'}>访客{visitorId}</span>
-          &nbsp;来到 BBS 社区，目前有 - 人与你同在，期待你成为社区一员，一起加入BBS讨论～
+          &nbsp;来到 BBS 社区，目前有 0 人与你同在，期待你成为社区一员，一起加入BBS讨论～
         </p>}
       </Marquee>
 
