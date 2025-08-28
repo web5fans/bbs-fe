0ab9c6f0b9d288509f31e86da5f9230530d87d25
chenhui <274241365@qq.com>
2025-08-28 17:19:06 +0800

    fix: postcss round

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,5 @@
 const config = {
-  plugins: ["@tailwindcss/postcss", "@csstools/postcss-stepped-value-functions"],
+  plugins: ["@csstools/postcss-stepped-value-functions", "@tailwindcss/postcss"],
 };
 
 export default config;
