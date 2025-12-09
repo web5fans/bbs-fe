8eaa20ef0031c3f84b628cc5528df761b789aca4
chenhui <274241365@qq.com>
2025-12-09 13:54:15 +0800

    feat: 帖子详情移动端UI优化

---
+++ src/app/posts/[postId]/_components/PostItem/_components/PostItemUserMobile/index.tsx
@@ -0,0 +1,63 @@
+import S from "./index.module.scss";
+import { PostItemType } from "@/app/posts/[postId]/_components/PostItem/index";
+import Avatar from "@/components/Avatar";
+import { useRouter } from "next/navigation";
+import cx from "classnames";
+import { useEffect, useState } from "react";
+
+const PostItemUserMobile = ({ post, isOriginPoster }: { post: PostItemType; isOriginPoster?: boolean }) => {
+  const [_, setRefresh] = useState(0)
+  const router = useRouter()
+
+  const href = `/user-center/${encodeURIComponent(post.author?.did)}`
+
+  const nickname = post.author?.displayName
+
+  const goUserCenter = (author) => {
+    if (!author.handle) return
+    router.push(href)
+  }
+
+  const name = !post.author?.handle ? '已注销' : nickname
+
+  useEffect(() => {
+    const f = () => {
+      requestAnimationFrame(() => {
+        setRefresh(v => v + 1)
+      })
+    }
+
+    window.addEventListener('resize', f);
+
+    return () => {
+      window.removeEventListener('resize', f);
+    }
+
+  }, []);
+
+  if (window.innerWidth > 768) return null
+
+  return <div
+    className={S.wrap}
+    onClick={() => goUserCenter(post.author)}
+    onMouseEnter={() => router.prefetch(href)}
+  >
+    <div className={'flex items-center'}>
+      <div className={cx(S.avatarWrap, !isOriginPoster && S.normal)}>
+        <Avatar nickname={nickname} className={S.avatar} />
+        <img
+          src={'/assets/poster.png'}
+          alt=""
+          className={S.poster}
+        />
+      </div>
+      <div className={S.postInfo}>
+        <p>{name}</p>
+        <p>发帖数量: {post.author?.post_count}</p>
+      </div>
+    </div>
+    <div className={S.divide} />
+  </div>
+}
+
+export default PostItemUserMobile;
--- src/app/posts/[postId]/_components/PostItem/index.tsx
+++ src/app/posts/[postId]/_components/PostItem/index.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import S from './index.module.scss'
-import Avatar from "@/components/Avatar";
 import cx from "classnames";
-import { useRouter } from "next/navigation";
 import PostItemContent from "./PostItemContent";
 import { useEffect, useRef, useState } from "react";
-import remResponsive from "@/lib/rem-responsive";
 import PostItemFooter from "./_components/PostItemFooter";
+import PostItemUser from "./_components/PostItemUser";
+import PostItemUserMobile from "./_components/PostItemUserMobile";
 
 export type PostItemType = {
   cid: string
@@ -46,15 +45,6 @@ type PostItemProps = {
 const PostItem = (props: PostItemProps) => {
   const { postInfo = {} as PostItemType, floor, isOriginPoster, isAuthor, rootUri, rootDisabled } = props;
 
-  const router = useRouter()
-
-  const nickname = postInfo.author?.displayName
-
-  const href = `/user-center/${encodeURIComponent(postInfo.author?.did)}`
-
-  const ref = useRef<HTMLDivElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
-
   const [postDisabled, setPostDisabled] = useState(false)
 
   useEffect(() => {
@@ -65,161 +55,34 @@ const PostItem = (props: PostItemProps) => {
     setPostDisabled(!postDisabled)
   }
 
-  // useEffect(() => {
-  //   const observer = new ResizeObserver(() => {
-  //     requestAnimationFrame(() => {
-  //       if (!ref.current) return
-  //       const height = ref.current.clientHeight
-  //       contentRef.current.style.setProperty('--height', `${height}px`)
-  //     })
-  //   })
-  //
-  //   observer.observe(ref.current)
-  //
-  //   return () => {
-  //     if (!ref.current) return
-  //     observer.unobserve(ref.current)
-  //   }
-  // }, []);
-
-  const goUserCenter = (author) => {
-    if (!author.handle) return
-    router.push(href)
-  }
-
   const disabled = rootDisabled || postDisabled
 
   return <div className={S.wrap}>
-    <div
-      ref={ref}
-      className={S.user}
-      onClick={() => goUserCenter(postInfo.author)}
-      onMouseEnter={() => router.prefetch(href)}
-    >
-      <UserAvatar nickname={nickname} isOriginPoster={isOriginPoster} isLoggedOff={!postInfo.author?.handle} />
-      <div className={S.divide} />
-      <p className={S.postNum}>
-        <span>发帖数量</span>
-        <span>{postInfo.author?.post_count}</span>
-      </p>
-    </div>
 
-    <div className={S.content} ref={contentRef}>
-      {disabled && <div className={cx(S.mask, rootDisabled && '!z-5')} />}
-      <div className={S.contentInner}>
-        <PostItemContent
-          postInfo={postInfo}
-          isAuthor={isAuthor}
+    <PostItemUser post={postInfo} isOriginPoster={isOriginPoster} />
+
+    <div className={S.contentWrap}>
+      <PostItemUserMobile post={postInfo} isOriginPoster={isOriginPoster} />
+      <div className={S.content}>
+        {disabled && <div className={cx(S.mask, rootDisabled && '!z-5')} />}
+        <div className={S.contentInner}>
+          <PostItemContent
+            postInfo={postInfo}
+            isAuthor={isAuthor}
+            rootUri={rootUri}
+            refresh={props.refresh}
+          />
+        </div>
+        <PostItemFooter
+          postInfo={{...postInfo, is_disabled: postDisabled}}
+          floor={floor}
           rootUri={rootUri}
           refresh={props.refresh}
+          switchPostVisibility={switchPostVisibility}
         />
       </div>
-      <PostItemFooter
-        postInfo={{...postInfo, is_disabled: postDisabled}}
-        floor={floor}
-        rootUri={rootUri}
-        refresh={props.refresh}
-        switchPostVisibility={switchPostVisibility}
-      />
     </div>
   </div>
 }
 
 export default PostItem;
-
-let resizeObserver: ResizeObserver | null = null;
-
-function UserAvatar({ isOriginPoster, nickname, isLoggedOff }: {
-  isOriginPoster?: boolean
-  nickname: string
-  isLoggedOff?: boolean
-}) {
-  const avatarRef = useRef<HTMLDivElement>(null);
-  const nameRef = useRef<HTMLParagraphElement>(null);
-  const name = isLoggedOff ? '已注销' : nickname
-
-  useEffect(() => {
-    if (!avatarRef.current || !name) return
-    const tempSpan = document.createElement("span");
-    tempSpan.innerText = name;
-    tempSpan.style.cssText = `
-        width: fit-content;
-        font-family: ${getComputedStyle(nameRef.current).fontFamily};
-        font-size: ${remResponsive(9)};
-      `;
-
-    document.body.appendChild(tempSpan);
-    const textWidth = tempSpan.clientWidth;
-    const defaultSize = getComputedStyle(tempSpan).fontSize.replace('px', '');
-
-    document.body.removeChild(tempSpan);
-
-    const resizeFont = () => {
-      resizeObserver = new ResizeObserver((entries) => {
-        requestAnimationFrame(() => {
-          if (!nameRef.current || !avatarRef.current) return
-
-          // 获取容器和文本宽度
-          const containerWidth = avatarRef.current?.scrollWidth;
-
-          if (!containerWidth) return
-
-          if (textWidth > containerWidth) {
-            // 计算需要缩小的比例
-            const ratio = containerWidth / textWidth;
-            let newSize = Math.floor(defaultSize * ratio) - 1;
-
-            // 设置最小字体限制
-            newSize = Math.max(newSize, 8);
-
-            // 应用新字体大小
-            nameRef.current.style.fontSize = newSize + 'px';
-            nameRef.current.style.width = containerWidth + 'px';
-            nameRef.current.style.whiteSpace = 'normal';
-            nameRef.current.style.height = 'auto';
-          } else {
-            nameRef.current.style.width = 'auto';
-            nameRef.current.style.height = 'auto';
-          }
-        })
-
-
-      })
-      resizeObserver.observe(avatarRef.current)
-    }
-
-    resizeFont()
-
-    const f = () => {
-      nameRef.current.style.height = nameRef.current.clientHeight + 'px';
-      nameRef.current.style.width = '0px';
-      resizeObserver?.unobserve(avatarRef.current)
-      resizeFont()
-    }
-
-    window.addEventListener('resize', f);
-
-    return () => {
-      resizeObserver?.disconnect()
-      window.removeEventListener('resize', f);
-    }
-  }, [name]);
-
-  return <>
-    <div className={cx(S.avatarWrap, !isOriginPoster && S.normal)} ref={avatarRef}>
-      <Avatar
-        nickname={nickname}
-        className={S.avatar}
-      />
-      <img
-        src={'/assets/poster.png'}
-        alt=""
-        className={S.poster}
-      />
-    </div>
-    <p
-      className={S.title}
-      ref={nameRef}
-    >{name}</p>
-  </>
-}
